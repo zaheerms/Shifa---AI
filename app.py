44c5b9e527dbae6154aec43f3c21dfaffe653455
Mohamed Zaheerudeen M S <147479150+zaheerms@users.noreply.github.com>
2025-02-12 12:17:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ parkinsons_model = load_model('parkinsons_model.sav')
 
 # Sidebar for navigation
 with st.sidebar:
-    selected = option_menu('Multiple Disease Prediction System',
+    selected = option_menu('Shifa - AI',
                            ['Diabetes Prediction', 'Heart Disease Prediction', 'Parkinsons Prediction'],
                            menu_icon='hospital-fill',
                            icons=['activity', 'heart', 'person'],

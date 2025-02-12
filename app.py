211e3d18450c86ab1c6454fa9a69b1e8c9f27ede
Mohamed Zaheerudeen M S <147479150+zaheerms@users.noreply.github.com>
2025-02-12 12:43:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ parkinsons_model = load_model('parkinsons_model.sav')
 
 # Sidebar for navigation
 with st.sidebar:
-    selected = option_menu("Zaheer's Prediction System Using ML 👇     Know Health on your Own😎",
+    selected = option_menu("Zaheer's Prediction System Using ML 👇                                 Know Health on your Own😎",
                            ['Diabetes Prediction', 'Heart Disease Prediction', 'Parkinsons Prediction'],
                            menu_icon='hospital-fill',
                            icons=['activity', 'heart', 'person'],
